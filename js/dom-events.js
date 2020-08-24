7d1d9c51434b94b2f2f59910cd8b5183741d9d1a
Kenji <kenji.au9@gmail.com>
2020-08-24 09:39:06 -0600

    Check JS file for active

---
+++ js/dom-events.js
@@ -0,0 +1,4 @@
+// First things first.. is our file loading into the browser?
+// alert( "DOM EVENTS JS LOADED" );
+// console.log( "DOM EVENTS JS LOADED" );
+// Normally the above is removed once you know its working.
