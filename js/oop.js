ac2cf8f9dc32a986241381f7a1cbde6344f82e8c
Kenji <kenji.au9@gmail.com>
2020-08-25 09:46:48 -0600

    Initial OOP

---
+++ js/oop.js
@@ -0,0 +1,10 @@
+// Basic JS object
+const myObj = {
+  name: "Joe",
+  age: 35,
+  hobbies: [ "Movies", "Pottery" ]
+};
+
+console.log( `My name is ${myObj.name}. I am ${myObj.age} years old!` );
+
+// JSON (JavaScript Object Notation)
