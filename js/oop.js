f69bab9e4d383e2aa0d0beb93714a65bc97b685a
Kenji <kenji.au9@gmail.com>
2020-08-25 09:59:07 -0600

    JSON Parse

--- js/oop.js
+++ js/oop.js
@@ -6,5 +6,19 @@ const myObj = {
 };
 
 console.log( `My name is ${myObj.name}. I am ${myObj.age} years old!` );
-
+console.log( myObj );
 // JSON (JavaScript Object Notation)
+// Properties MUST be wrapped in double quotes to be considered valid JSON.
+// We CANNOT have methods in a JSON string/Object.
+const secondPersonString = `{
+  "name": "Sarah",
+  "age": 29,
+  "hobbies": [ "Deep sea fishing", "Cycling" ]
+}`;
+
+console.log( secondPersonString );
+// Convert a string into regular JS object we can use.
+const secondPersonObj = JSON.parse( secondPersonString );
+// If the string was properly formatted JSON, it should now be usable!
+// It will throw an error if not!
+console.log( secondPersonObj );
